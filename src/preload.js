daf81ccaa049d77ac8ec55190602b400f619c9a2
Jordan Nonagni <nonagnijordan@gmail.com>
2025-04-29 15:22:48 +0100

    restart from scratch

--- src/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer, desktopCapturer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    getSources: (options) => desktopCapturer.getSources(options),
-    saveFile: (buffer) => ipcRenderer.invoke('save-file', buffer)
-});
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
